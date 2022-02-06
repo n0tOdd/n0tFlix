4d7c994063c0491aad6b2d911cf9643b3d93862b
n0tOdd <oddostlie@gmail.com>
2022-02-06 05:51:03 +0100

    testing youtubedl installer

--- API/n0tFlix.Plugin.YoutubeDL/YoutubeDlEntryPoint.cs
+++ API/n0tFlix.Plugin.YoutubeDL/YoutubeDlEntryPoint.cs
@@ -58,7 +58,7 @@ namespace n0tFlix.Plugin.YoutubeDL
                 {
                     Logger.LogDebug("Downloading youtube-dl");
                     Stream youtubeDL = await httpclient.GetStreamAsync("https://yt-dl.org/downloads/latest/youtube-dl");
-                    using (var fs = new FileStream(Path.Combine(Plugin.Instance.AssemblyFilePath, "youtube-dl"), FileMode.CreateNew))
+                    using (var fs = new FileStream(Path.Combine("/var/lib/jellyfin/plugins/YoutubeDL_0.0.0.1/", "youtube-dl"), FileMode.CreateNew))
                     {
                         await youtubeDL.CopyToAsync(fs);
                     }
