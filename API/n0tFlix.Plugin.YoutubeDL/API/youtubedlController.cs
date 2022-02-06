28762095a3681a51bc0fec1afa391a9817ba226a
n0tOdd <oddostlie@gmail.com>
2022-02-06 13:22:34 +0100

    I think youtubedl is working now

--- API/n0tFlix.Plugin.YoutubeDL/API/youtubedlController.cs
+++ API/n0tFlix.Plugin.YoutubeDL/API/youtubedlController.cs
@@ -53,9 +53,9 @@ namespace n0tFlix.Plugin.YoutubeDL.API
             NYoutubeDL.YoutubeDL youtubeDL = new NYoutubeDL.YoutubeDL();
       //      YoutubeDL youtubeDL = new YoutubeDL("/var/lib/jellyfin/plugins/YoutubeDL_1.0.0.0/youtube-dl");
             youtubeDL.Options.VerbositySimulationOptions.GetUrl = true;
-            /*youtubeDL.Options.VerbositySimulationOptions.Simulate = true;
+            youtubeDL.Options.VerbositySimulationOptions.Simulate = true;
             youtubeDL.Options.VerbositySimulationOptions.SkipDownload = true;
-            youtubeDL.Options.VerbositySimulationOptions.DumpSingleJson = true;
+            /*youtubeDL.Options.VerbositySimulationOptions.DumpSingleJson = true;
             youtubeDL.Options.VerbositySimulationOptions.PrintJson = true;
             youtubeDL.Options.GeoRestrictionOptions.GeoBypass = true;
             
